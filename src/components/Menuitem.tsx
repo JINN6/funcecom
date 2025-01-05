ee92a18e267e226033e315e2e348eeec22bdab26
Faiz <fk671384@gmail.com>
2025-01-05 03:42:28 -0800

    Updated files or added new features

--- src/components/Menuitem.tsx
+++ src/components/Menuitem.tsx
@@ -77,6 +77,8 @@ const ProductCardGrid = () => {
   return (
     <div className="relative text-center p-10 bg-cover bg-center bg-gray-100 mt-[60px]">
       <h1 className="text-4xl font-bold text-gray-800 mb-8">Hot Selling Items!</h1>
+      
+      {/* Product Grid */}
       <section className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 justify-items-center mt[60px]">
         {products.map((product) => (
           <div
